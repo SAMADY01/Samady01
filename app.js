fee6e3bdb57558fb2178978a3c83aeb743ac3533
SAMADY01 <125808846+SAMADY01@users.noreply.github.com>
2023-02-20 15:27:31 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var humeur= prompt("comment allez vous");
+var prenom= prompt("Dites moi votre prenom");
+var phrase='Donc vous vous appellez' + 'prenom'+ et vous allez' + 'humeur' );
+alert("phrase")
